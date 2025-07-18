52b703d3f73d4074662a14003ec0988bf7db543f
Alejo <alejomesa.999@gmail.com>
2025-07-18 16:00:00 -0500

    feat: create pages, define routes, and implement dynamic navbar using titles from appRoutes

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,40 @@
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map } from 'rxjs';
+import { routes } from '../../../app.routes';
+@Component({
+  selector: 'app-navbar',
+  imports: [AsyncPipe, RouterLink],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+  router = inject(Router);
+
+  routes = routes
+    .map((route) => ({
+      path: route.path,
+      title: `${route.title ?? 'Maps en Angular'}`,
+    }))
+    .filter((route) => route.path != '**');
+
+  pageTitle$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    map((event) => event.url),
+    map(
+      (url) =>
+        routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas'
+    )
+  );
+
+  // pageTitles = toSignal(
+  //   this.router.events.pipe(
+  //     filter((event) => event instanceof NavigationEnd),
+  //     map((event) => event.url),
+  //     map(
+  //       (url) =>
+  //         routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas'
+  //     )
+  //   )
+  // );
+}
